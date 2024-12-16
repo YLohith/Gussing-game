d30f15d5074a8f80850cb4aedd81d6089a5de5e2
YLohith <127972406+YLohith@users.noreply.github.com>
2024-12-16 04:26:28 -0800

    Add project files.

---
+++ Gussing game/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Gussing_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuess = false;
+
+            while(guess != secretWord && !outOfGuess)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                } 
+                else
+                {
+                    outOfGuess = true;
+                }  
+            }
+            if (outOfGuess)
+            {
+                Console.WriteLine("YOU Lose!");
+            }
+            else
+            {
+                Console.WriteLine("YOU WIN!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
